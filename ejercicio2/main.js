e470a33c85fc00df0dd6981fcc86c401d3dfae46
Agustin.- <107271794+agus94lp@users.noreply.github.com>
2022-11-09 20:16:57 -0300

    Ejercicio2
    2

---
+++ ejercicio2/main.js
@@ -0,0 +1,10 @@
+
+
+const lista = ["Agustin", 28, false, new Date(1994, 10, 14),
+{
+    titulo: "Vuelta al mundo en 80 dias",
+    autor: "Julio Verne",
+    fecha: new Date(1872, 12, 22),
+    url: "https://www.argentina.gob.ar/sites/default/files/verne_julio_-_la_vuelta_al_mundo_en_80_dias.pdf"
+}
+];
