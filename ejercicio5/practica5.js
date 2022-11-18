b0b8e1877abcaf2e1d4c36dbd619a149cf68b577
Agustin.- <107271794+agus94lp@users.noreply.github.com>
2022-11-18 19:17:19 -0300

    practica 5
    ejercicio 5, numeros

---
+++ ejercicio5/practica5.js
@@ -0,0 +1,13 @@
+let alturaCent = 165;
+let alturaMetro = 1.65;
+
+let peso = 65.5;
+
+let alturaArriba = Math.ceil(alturaMetro);
+let pesoAbajo = Math.floor(peso);
+
+let igual = Number.MAX_VALUE +1 === Number.MAX_VALUE;
+
+console.log(alturaArriba);
+console.log(pesoAbajo);
+console.log(igual);
