5eb855687e413e44225433a6f24c0bc26fafdbc7
Agustin.- <107271794+agus94lp@users.noreply.github.com>
2023-07-12 22:53:03 -0300

    ejercicio14

---
+++ ejercicio14/index.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", ()=>{
+    alert("click en el botón");
+})
+
+$('.boton').click(function(){
+    console.log("Hola, estoy utilizando jQuery");
+})
